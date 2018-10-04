07477c1eef4937c91d0472e6d1879c4d8bdf6e3e
Loner1024 <xth12138@gmail.com>
2018-10-05 02:12:44 +0800

    '一个JavaScripts写的爬虫，包含HTML写的图形界面，用来爬取云南招考网'

---
+++ xwwzphoto/function.js
@@ -0,0 +1,309 @@
+var a = 0;
+function photo1() {
+    while (a < 9) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/299y00" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+    while (a < 76) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/299y0" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+}
+function photo2() {
+    while (a < 9) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/300y00" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+    while (a < 88) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/300y0" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+} function photo3() {
+    while (a < 9) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/301y00" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+    while (a < 75) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/301y0" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+} function photo4() {
+    while (a < 9) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/302y00" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+    while (a < 72) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/302y0" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+} function photo5() {
+    while (a < 9) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/303y00" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+    while (a < 76) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/303y0" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+} function photo6() {
+    while (a < 9) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/304y00" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+    while (a < 72) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/304y0" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+} function photo7() {
+    while (a < 9) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/305y00" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+    while (a < 69) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/305y0" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+} function photo8() {
+    while (a < 9) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/306y00" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+    while (a < 76) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/306y0" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+} function photo9() {
+    while (a < 9) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/307y00" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+    while (a < 100) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/307y0" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+} function photo10() {
+    while (a < 9) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/308y00" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+    while (a < 79) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/308y0" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+} function photo11() {
+    while (a < 9) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/309y00" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+    while (a < 72) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/309y0" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+} function photo12() {
+    while (a < 9) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/310y00" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+    while (a < 74) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/310y0" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+} function photo13() {
+    while (a < 9) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/311y00" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+    while (a < 72) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/311y0" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+} function photo14() {
+    while (a < 9) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/312y00" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+    while (a < 76) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/312y0" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+} function photo15() {
+    while (a < 9) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/313y00" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+    while (a < 66) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/313y0" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+} function photo16() {
+    while (a < 9) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/314y00" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+    while (a < 63) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/314y0" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+} function photo17() {
+    while (a < 9) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/315y00" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+    while (a < 64) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/315y0" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+} function photo18() {
+    while (a < 9) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/316y00" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+    while (a < 63) {
+        a = a + 1;
+        var url = "http://work.ynzs.cn/ZSGL/servletphoto?path=kszp/22240205/316y0" + a + ".jpg";
+
+        //document.write(url + ",");
+        document.write("<img src=" + url + ">")
+
+    }
+}
