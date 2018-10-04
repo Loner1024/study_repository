d9494bcd1a7a0404addfc7e09a495132c2fd0681
Loner1024 <xth12138@gmail.com>
2018-10-05 02:18:03 +0800

    '旧的Flask学习笔记，已经弃坑'

---
+++ flask_study/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+#from flask import *
+from flask import render_template
+
+app = Flask(__name__)
+'''
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+'''
+@app.route('/<name>')
+def hello(name):
+    return render_template('index.html', name=name)
+
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run()
